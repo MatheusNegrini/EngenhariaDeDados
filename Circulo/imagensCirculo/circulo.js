d4c31dd90f136b1017761014f9d76378eb1f6720
Matheus Negrini da Costa <100143897+MatheusNegrini@users.noreply.github.com>
2024-08-15 10:23:23 -0300

    Melhoramento do index.html

---
+++ Circulo/imagensCirculo/circulo.js
@@ -0,0 +1,12 @@
+function calcularRaio() {
+    let raio = document.getElementById("raio").value;
+    let area = 3.14 * (raio * raio);
+
+    document.getElementById("resultado_raio").innerHTML = `Á área do Círculo é: ${area} cm` 
+}
+
+function calcularDiametro(){
+    let raio = document.getElementById("raio").value;
+    let diametro = raio * 2;
+    document.getElementById("resultado_diametro").innerHTML = `O diâmetro do Círculo é: ${diametro} cm`
+}
--- main.js
+++ main.js
@@ -1,17 +1,11 @@
+const square = document.querySelector(".square")
+const wrapper = document.querySelector(".wrapper")
+const teste = document.querySelector(".hiddenCalculate")
 
+console.log(teste)
 
-
-/* Parte onde se refere à circulo.html */
-
-function calcularRaio() {
-    let raio = document.getElementById("raio").value;
-    let area = 3.14 * (raio * raio);
-
-    document.getElementById("resultado_raio").innerHTML = `Á área do Círculo é: ${area} cm` 
-}
-
-function calcularDiametro(){
-    let raio = document.getElementById("raio").value;
-    let diametro = raio * 2;
-    document.getElementById("resultado_diametro").innerHTML = `O diâmetro do Círculo é: ${diametro} cm`
+function calculateLayout(){
+    wrapper.classList.add("hidden")
+    teste.classList.add("showCalculate")
+    teste.classList.remove("hiddenCalculate")
 }

b4008a9a4047474155e63598240512e1f6105054
Matheus Negrini da Costa <100143897+MatheusNegrini@users.noreply.github.com>
2024-09-05 20:34:04 -0300

    melhoramento do circulo.js e o main.js
    Bug fix de erro na mensagem

--- main.js
+++ main.js
@@ -1,58 +1,47 @@
-const square = document.querySelector(".square")
-const wrapper = document.querySelector(".wrapper")
-const teste = document.querySelector(".hiddenCalculateSquare")
-const back = document.querySelector(".backToMenu")
-let base = document.querySelector(".inputBase")
-let altura = document.querySelector(".inputAltura")
-let alturaValue = document.querySelector(".heightSquare")
-let baseValue = document.querySelector(".baseSquare")
+const square = document.querySelector(".square");
+const wrapper = document.querySelector(".wrapper");
+const testeSquare = document.querySelector(".hiddenCalculateSquare");
+const back = document.querySelector(".backToMenu");
+let base = document.querySelector(".inputBase");
+let altura = document.querySelector(".inputAltura");
+let alturaValue = document.querySelector(".heightSquare");
+let baseValue = document.querySelector(".baseSquare");
 
-
-function calculateLayoutSquare(){
-    wrapper.classList.add("hidden")
-    teste.classList.add("showCalculateSquare")
-    teste.classList.remove("hiddenCalculateSquare")
+function calculateLayoutSquare() {
+    wrapper.classList.add("hidden");
+    testeSquare.classList.add("showCalculateSquare");
+    testeSquare.classList.remove("hiddenCalculateSquare");
 }
 
-function mainLayout(){
-    wrapper.classList.remove("hidden")
-    wrapper.classList.add("wrapper")
-    teste.classList.add("hiddenCalculateSquare")
-    teste.classList.remove("showCalculateSquare")
+function mainLayout() {
+    wrapper.classList.remove("hidden");
+    wrapper.classList.add("wrapper");
+    testeSquare.classList.add("hiddenCalculateSquare");
+    testeSquare.classList.remove("showCalculateSquare");
 }
 
+function calcSquare() {
+    let isNumberHeight = altura.value;
+    let isNumberBase = base.value;
 
-function calcSquare(){
-    let isNumberHeight = altura.value
-    let isNumberBase = base.value
-
-
-    function checkIsNumber(){
-        return !isNaN(isNumberHeight) && !isNaN(isNumberBase)
+    function checkIsNumber() {
+        return !isNaN(isNumberHeight) && !isNaN(isNumberBase);
     }
 
-    if(altura.value == '' || base.value == ''){
-        return alert("Preencha todos os campos")
-    } else if(checkIsNumber){
-        return alert("Digite apenas números")
+    if (altura.value === '' || base.value === '') {
+        return alert("Preencha todos os campos");
+    } else if (!checkIsNumber()) {
+        return alert("Digite apenas números");
     }
 
-    numberCheck = altura.value.split('')
-    if(numberCheck.length > 8){
-        return alert("Digite um valor com no máximo 8 digitos")
+    let numberCheck = altura.value.split('');
+    if (numberCheck.length > 8) {
+        return alert("Digite um valor com no máximo 8 dígitos");
     }
-    
-    if(alturaValue.innerHTML == 'Altura:'){
-    alturaValue.innerHTML = heightSquareNumber
-} else {
-    alturaValue.innerHTML = ''
-    alturaValue.innerHTML = heightSquareNumber
-    }
-}
 
-if(baseValue.innerHTML == 'Base:'){
-    baseValue.innerHTML = baseSquareNumber
-} else {
-    baseValue.innerHTML = ''
-    baseValue.innerHTML = baseSquareNumber
+    alturaValue.innerHTML = `Altura: ${altura.value}`;
+    baseValue.innerHTML = `Base: ${base.value}`;
+
+    let area = altura.value * base.value;
+    document.querySelector(".areaSquare").innerHTML = `Área: ${area}`;
 }
